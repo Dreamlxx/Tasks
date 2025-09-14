b654a4d25b2d76772b9435a6957694a07fac63c7
Dreamlx <1773632641@qq.com>
2025-09-14 17:20:32 +0800

    11

---
+++ total/java1/课程代码/Text2.java
@@ -0,0 +1,11 @@
+public class Text2 {
+    public static void main(String[] args) {
+        char a = 'a';
+        char b = '中';
+        char c = '\'';//想要输入’必须加个\
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
